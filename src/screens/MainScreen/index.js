100726626c48420c06cb184c599f36fab03b95f0
fatihk29 <fatihkrc7@gmail.com>
2021-08-26 22:31:24 +0300

    updated

--- src/screens/MainScreen/index.js
+++ src/screens/MainScreen/index.js
@@ -1,19 +1,87 @@
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
+import data from '../../helpers/data';
 
-const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
+const locations = [
+  {
+    title: 'Location 1',
+    latitude: 24.86170245,
+    longitude: 67.00310938,
+    markerImage: 'https://i.ibb.co/2PwKJWc/UA-Studios-2.png',
+    weight: 13,
   },
-  map: {
-    ...StyleSheet.absoluteFillObject,
+  {
+    title: 'Location 2',
+    latitude: 24.8317098,
+    longitude: 67.00210948,
+    markerImage: 'https://i.ibb.co/2PwKJWc/UA-Studios-2.png',
+    weight: 19,
   },
-});
-
+  {
+    title: 'Location 3',
+    latitude: 24.83073537,
+    longitude: 67.02129903,
+    markerImage: 'https://i.ibb.co/2PwKJWc/UA-Studios-2.png',
+    weight: 18,
+  },
+  {
+    title: 'Location 4',
+    latitude: 24.8307323,
+    longitude: 67.10113298,
+    markerImage: 'https://i.ibb.co/2PwKJWc/UA-Studios-2.png',
+    weight: 12,
+  },
+  {
+    title: 'Location 5',
+    latitude: 24.8307999,
+    longitude: 67.0293998,
+    markerImage: 'https://i.ibb.co/2PwKJWc/UA-Studios-2.png',
+    weight: 2,
+  },
+  {
+    title: 'Location 6',
+    latitude: 24.85072329,
+    longitude: 67.02129803,
+    markerImage: 'https://i.ibb.co/2PwKJWc/UA-Studios-2.png',
+    weight: 15,
+  },
+  {
+    title: 'Location 7',
+    latitude: 24.84089002,
+    longitude: 67.02122203,
+    markerImage: 'https://i.ibb.co/2PwKJWc/UA-Studios-2.png',
+    weight: 5,
+  },
+  {
+    title: 'Location 8',
+    latitude: 24.84064338,
+    longitude: 67.031209,
+    markerImage: 'https://i.ibb.co/2PwKJWc/UA-Studios-2.png',
+    weight: 50,
+  },
+  {
+    title: 'Location 9',
+    latitude: 24.8405889,
+    longitude: 67.04114039,
+    markerImage: 'https://i.ibb.co/2PwKJWc/UA-Studios-2.png',
+    weight: 54,
+  },
+  {
+    title: 'Location 10',
+    latitude: 24.8505999,
+    longitude: 67.04139399,
+    markerImage: 'https://i.ibb.co/2PwKJWc/UA-Studios-2.png',
+    weight: 34,
+  },
+  {
+    title: 'Location 11',
+    latitude: 24.85034563,
+    longitude: 67.04111009,
+    markerImage: 'https://i.ibb.co/2PwKJWc/UA-Studios-2.png',
+    weight: 21,
+  },
+];
 
 export default () => (
   <View style={styles.container}>
@@ -21,11 +89,36 @@ export default () => (
       provider={PROVIDER_GOOGLE}
       style={styles.map}
       region={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.015,
-        longitudeDelta: 0.0121,
-      }}
-    />
+        latitude: 24.8307323,
+        longitude: 67.10113298,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}>
+      {locations.map(marker => {
+        return (
+          <Marker
+            coordinate={{
+              latitude: marker.latitude,
+              longitude: marker.longitude,
+            }}
+            title={marker.title}
+          />
+        );
+      })}
+    </MapView>
   </View>
 );
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    // flex: 1,
+    width: '100%',
+    height: '90%',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
